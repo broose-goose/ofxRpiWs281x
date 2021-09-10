a9b22a564b627c97670bfbebed4fd33fded3deae
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2021-09-10 18:14:56 -0500

    back to segfaults

--- src/ofxRpiWs281x.cpp
+++ src/ofxRpiWs281x.cpp
@@ -60,7 +60,8 @@ namespace ofxRpiWs281x {
 
     void LedStrip::SetColorPixel(ofColor c, uint16_t pixel) {
         if (pixel < _channel.count) {
-            _channel.leds[pixel] = wrgbFromOfColor(c);
+            _strip.channel[0].leds[0] = 0x00200000;
+            // _channel.leds[pixel] = wrgbFromOfColor(c);
         } else {
             std::cout << "Pixel out of range" << std::endl;
         }
