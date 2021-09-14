edc0469d98286a2fb4bf84db162bd27d7961c8be
Robert B Wilcoxon III <rbwilcoxon11@utexas.edu>
2021-09-14 14:39:37 -0500

    welp

--- src/ofxRpiWs281x.cpp
+++ src/ofxRpiWs281x.cpp
@@ -100,6 +100,10 @@ namespace ofxRpiWs281x {
 
     ReturnValue LedStrip::Render() {
 
+        for (auto &pix : _pixels) {
+            std::cout << (int)pix.r << ", " (int)pix.g << ", " (int) pix.b << std::endl;
+        }
+
 #ifdef __arm__
         if (!_is_initialized) {
             std::cout << "LedStrip, Render: can't render shit..." << std::endl;
